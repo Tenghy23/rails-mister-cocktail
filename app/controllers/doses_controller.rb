389de0e4de42c6174335eeb429de05d4fb89a5b9
Hui Yong Teng <tenghuiyong@outlook.sg>
2020-11-27 04:31:07 +0800

    mister cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,28 +6,28 @@ class DosesController < ApplicationController
   end
 
   def create
-    @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = cocktail
+    @dose = Dose.new(doses_params) # dose params take data from the form in create
+    @cocktail = Cocktail.find(params[:cocktail_id]) # this finds the cocktail instance via id
+    @dose.cocktail = @cocktail # saves the cocktail instance inside dose
     @dose.save
 
     if @dose.save
-
     else
       # borrowing the new page and showing it
       render :new
     end
-    redirect_to cocktail_path(cocktail)
   end
 
   def destroy
-  
+    @dose = dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path()
   end
 
   private
 
   def doses_params
     # :dose refers to key in params
-    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
